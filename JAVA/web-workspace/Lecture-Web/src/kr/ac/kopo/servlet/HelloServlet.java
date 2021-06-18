ec3b5875b8eded481f9866a750ca8193a75979f4
Bonseong <9bonseong@naver.com>
2021-06-18 16:37:46 +0900

    210618 commit

--- JAVA/web-workspace/Lecture-Web/src/kr/ac/kopo/servlet/HelloServlet.java
+++ JAVA/web-workspace/Lecture-Web/src/kr/ac/kopo/servlet/HelloServlet.java
@@ -1,7 +1,26 @@
 package kr.ac.kopo.servlet;
 
-import javax.servlet.http.HttpServlet;
+import java.io.IOException;
 
-public class HelloServlet extends HttpServlet{
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
 	
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("init() : 최초 한번만 실행되는 메소드입니다.");
+
+	}
+	
+
+	public void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException /* 파일 입출력에 대한 간접처리 */ {
+		System.out.println("service() : 실제 작업이 실행되는 메소드입니다.");
+		System.out.println("사용자가 요청할 때마다 호출되는 메소드입니다.");
+
+	}
 }
---
+++ JAVA/web-workspace/Lecture-Web/src/kr/ac/kopo/servlet/MethodServlet.java
@@ -0,0 +1,53 @@
+package kr.ac.kopo.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MethodServlet extends HttpServlet {
+
+	// GET http://localhost:9999/Lecture-Web/methods?id=aaa
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String id = request.getParameter("id"); // method id가 던져주는 값
+		String method = request.getMethod();
+		StringBuffer url = request.getRequestURL(); /* URL : 프로토콜 포함 */
+		String uri = request.getRequestURI();
+
+		/*
+		 * // 서버 출력 System.out.println("id : " + id); System.out.println("method : " +
+		 * method); System.out.println("url : " + url.toString());
+		 * System.out.println("uri : " + uri);
+		 */
+
+		// client에게 응답 (outputstream이나 writer 필요!)
+
+		response.setContentType("text/html; charset=utf-8"); // 받아올 데이터에 대한 형식
+		PrintWriter out = response.getWriter();
+		
+		out.println("<html>");
+		out.println("	<head>");
+		out.println("		<title>메소드 호출방식</title>");
+		out.println("	</head>");
+		out.println("	<body>");
+		out.println("	</body>");
+		out.println("===============================<br>");
+		out.println("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;요 청 결 과<br>");
+		out.println("===============================<br>");
+		out.println("파라미터(id) : " + id +  "<br>");
+		out.println("요청방식 : " + method +  "<br>");
+		out.println("요청uri : " + uri +  "<br>");
+		out.println("===============================<br>");
+
+		out.println("</html>");
+
+		out.flush(); // flush : FileWriter 내부 버퍼의 내용을 파일에 write
+		out.close();
+
+	}
+}
