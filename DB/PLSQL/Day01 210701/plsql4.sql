c6f3b7cc0564b9f404edd15ddceda3189d003905
Bonseong <9bonseong@naver.com>
2021-07-01 13:59:40 +0900

    210701 commit

---
+++ DB/PLSQL/Day01 210701/plsql4.sql
@@ -0,0 +1,12 @@
+DECLARE
+    LOOP_INDEX  NUMBER(4)   :=  1;
+    MAX_LOOP_INDEX  NUMBER(4)   :=  30; -- 생략해도 되는 부분
+BEGIN
+    --FOR LOOP_INDEX  IN  1..30 -- 성공
+    --FOR LOOP_INDEX  IN  30..1
+    FOR LOOP_INDEX  IN  REVERSE 30..1 -- 성공
+    --FOR LOOP_INDEX  IN  REVERSE 1..30
+    LOOP
+        DBMS_OUTPUT.PUT_LINE('LOOP COUNT '||TO_CHAR(LOOP_INDEX));
+    END LOOP;
+END;    
