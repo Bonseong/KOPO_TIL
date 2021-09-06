b51f1af088babe0c02dc267815515a33acbdf48f
Bonseong <9bonseong@naver.com>
2021-09-06 09:05:36 +0900

    210906

--- JAVA/spring-workspace/DynamicCardStatement/src/main/java/kr/ac/kopo/card/service/CardServiceImpl.java
+++ JAVA/spring-workspace/DynamicCardStatement/src/main/java/kr/ac/kopo/card/service/CardServiceImpl.java
@@ -2,6 +2,7 @@ package kr.ac.kopo.card.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kr.ac.kopo.card.vo.CardVO;
@@ -9,9 +10,13 @@ import kr.ac.kopo.card.vo.CardVO;
 @Service
 public class CardServiceImpl implements CardService{
 
+	@Autowired
+	private CardDAO cardDAO;
+	
 	
 	public List<CardVO> selectCardList() {
 		
+		List<CardVO> cardList
 		return null;
 	}
 
